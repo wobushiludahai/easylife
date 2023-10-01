e9bd95bb05be78d5f8748f1c96f98d6e3e61e9f4
wobushiludahai <wobushiludahai@163.com>
2023-10-02 00:35:09 +0800

    feature: 重构highlightline

--- src/extension.ts
+++ src/extension.ts
@@ -1,99 +1,93 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 'use strict'
 import * as vscode from 'vscode';
 import { window, workspace, Range, Position, TextEditor } from 'vscode';
 
 import HighlightConfig from './config';
+import HighlightLine from './highlightline';
 
 export async function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "easylife" is now active!');
 
-	let decorationType = HighlightConfig.getHighlightLineConfig().decorators;
-	let activeEditor = window.activeTextEditor;
-	if (activeEditor === undefined) {
-		return
-	}
-	let lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+	new HighlightLine();
 
-	window.onDidChangeActiveTextEditor(() => {
-		try {
-			console.log("try test")
-            activeEditor = window.activeTextEditor
-            updateDecorations(decorationType)
-		} catch (e) {
-			console.error("Error from window.onDicChangeActiveTxEditor -->", e)
-		} finally {
-			if (activeEditor === undefined) {
-				return
-			}
-			console.log("final test finished")
-			lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
-		}
-	})
+	// let highlightLineConfig = HighlightConfig.getHighlightLineConfig();
+	// let decorationType = highlightLineConfig.decorators;
+	// let activeEditor = window.activeTextEditor;
+	// if (activeEditor === undefined) {
+	// 	return
+	// }
+	// let lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
 
-	window.onDidChangeTextEditorSelection(() => {
-		console.log("onDidChangeTextEditorSelection test")
-		activeEditor = window.activeTextEditor;
-		if (activeEditor == undefined) {
-			return
-		}
+	// window.onDidChangeActiveTextEditor(() => {
+	// 	try {
+    //         activeEditor = window.activeTextEditor
+    //         updateDecorations(decorationType)
+	// 	} catch (e) {
+	// 		console.error("Error from window.onDicChangeActiveTxEditor -->", e)
+	// 	} finally {
+	// 		if (activeEditor === undefined) {
+	// 			return
+	// 		}
+	// 		lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+	// 	}
+	// })
 
-		let word = activeEditor.document.getText(activeEditor.selection);
-		if (!word) {
-			console.log('Nothing selected');
-		} else {
-			console.log(word);
-		}
+	// window.onDidChangeTextEditorSelection(() => {
+	// 	activeEditor = window.activeTextEditor;
+	// 	if (activeEditor == undefined) {
+	// 		return
+	// 	}
 
-        updateDecorations(decorationType);
-	})
+	// 	let word = activeEditor.document.getText(activeEditor.selection);
+	// 	if (!word) {
+	// 		console.log('Nothing selected');
+	// 		return;
+	// 	} else {
+	// 		console.log(word);
+	// 	}
 
-    function updateDecorations(decorationType: any, updateAllVisibleEditors=false) {
-		try {
-			if (updateAllVisibleEditors) {
-				window.visibleTextEditors.forEach((editor) => {
-					const currentPosition = editor.selection.active;
-                    const currentLine = editor.selection.active.line;
-                    const newDecoration = { range: new Range(currentPosition, currentPosition) };
-                    editor.setDecorations(decorationType, [newDecoration]);
-                });
-            }
+    //     updateDecorations(decorationType);
+	// })
 
-            //edit only currently active editor
-            else {
-				window.visibleTextEditors.forEach((editor) => {
-					if(editor !== window.activeTextEditor || activeEditor === undefined) return;
+    // function updateDecorations(decorationType: any, updateAllVisibleEditors=false) {
+	// 	try {
+	// 		if (updateAllVisibleEditors) {
+	// 			window.visibleTextEditors.forEach((editor) => {
+	// 				const currentPosition = editor.selection.active;
+    //                 const currentLine = editor.selection.active.line;
+    //                 const newDecoration = { range: new Range(currentPosition, currentPosition) };
+    //                 editor.setDecorations(decorationType, [newDecoration]);
+    //             });
+    //         } else {
+	// 			window.visibleTextEditors.forEach((editor) => {
+	// 				if(editor !== window.activeTextEditor || activeEditor === undefined) return;
 
-                    const currentPosition = editor.selection.active
-                    const editorHasChangedLines = lastActivePosition.line !== currentPosition.line
-                    const isNewEditor = activeEditor.document.lineCount === 1 && lastActivePosition.line === 0 && lastActivePosition.character == 0;
-                    const newDecoration = { range: new Range(currentPosition, currentPosition) }
+    //                 const currentPosition = editor.selection.active
+    //                 const editorHasChangedLines = lastActivePosition.line !== currentPosition.line
+    //                 const isNewEditor = activeEditor.document.lineCount === 1 && lastActivePosition.line === 0 && lastActivePosition.character == 0;
+    //                 const newDecoration = { range: new Range(currentPosition, currentPosition) }
 
-                    if(editorHasChangedLines || isNewEditor){
-                        editor.setDecorations(decorationType, [newDecoration])
-                    }
-                });
-            }
-        }
-        catch (error){
-            console.error("Error from ' updateDecorations' -->", error)
-        } finally {
-			if (activeEditor === undefined) return;
+    //                 if(editorHasChangedLines || isNewEditor){
+    //                     editor.setDecorations(decorationType, [newDecoration])
+    //                 }
+    //             });
+    //         }
+    //     } catch (error){
+    //         console.error("Error from ' updateDecorations' -->", error)
+    //     } finally {
+	// 		if (activeEditor === undefined) return;
 
-            lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
-        }
-    }
+    //         lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+    //     }
+    // }
 
-    workspace.onDidChangeConfiguration(() => {
-        //clear all decorations
-        decorationType.dispose();
-        decorationType = HighlightConfig.getHighlightLineConfig().decorators;
-        updateDecorations(decorationType, true)
-    })
+    // workspace.onDidChangeConfiguration(() => {
+    //     decorationType.dispose();
+
+	// 	let highlightLineConfig = HighlightConfig.getHighlightLineConfig();
+    //     decorationType = highlightLineConfig.decorators;
+    //     updateDecorations(decorationType, true)
+    // })
 }
 
 // This method is called when your extension is deactivated
---
+++ src/highlightline.ts
@@ -0,0 +1,115 @@
+'use strict';
+import { window, Range, TextEditorDecorationType, TextEditor, Position, workspace } from "vscode";
+import HighlightConfig from './config';
+
+export interface HighLightable {
+    word: string;
+    iswholeWord: boolean;
+    ignoreCase: boolean;
+}
+
+class HighlightLine {
+    private decorationType: TextEditorDecorationType
+    private isHighlightEnable: boolean
+    private activeEditor: TextEditor | undefined
+    private lastActivePosition: Position | undefined
+
+    constructor() {
+        let highlightLineConfig = HighlightConfig.getHighlightLineConfig();
+
+        this.decorationType = highlightLineConfig.decorators;
+        this.isHighlightEnable = highlightLineConfig.isHighlightLineEnabled;
+        let activeEditor = window.activeTextEditor;
+        if (activeEditor) {
+            this.activeEditor = activeEditor;
+            this.lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+        }
+
+        this.registerHightline();
+    }
+
+    public registerHightline() {
+        // 编辑器选择
+        window.onDidChangeActiveTextEditor(() => {
+            if (this.isHighlightEnable === false) {
+                return;
+            }
+
+            try {
+                this.activeEditor = window.activeTextEditor
+                this.highlightLine(this.decorationType)
+            } catch (e) {
+                console.error("Error from window.onDicChangeActiveTxEditor -->", e)
+            } finally {
+                if (this.activeEditor === undefined) {
+                    return
+                }
+                this.lastActivePosition = new Position(this.activeEditor.selection.active.line, this.activeEditor.selection.active.character);
+            }
+        })
+
+        // 文本选择
+        window.onDidChangeTextEditorSelection(() => {
+            let activeEditor = window.activeTextEditor;
+            if (activeEditor === undefined) {
+                return
+            }
+
+            this.activeEditor = activeEditor
+            let word = activeEditor.document.getText(activeEditor.selection);
+            if (!word) {
+                console.log('Nothing selected');
+            } else {
+                console.log(word);
+            }
+
+            this.highlightLine(this.decorationType);
+        })
+
+        // 配置修改
+        workspace.onDidChangeConfiguration(() => {
+            this.decorationType.dispose();
+            let highlightLineConfig = HighlightConfig.getHighlightLineConfig();
+            this.decorationType = highlightLineConfig.decorators;
+            this.isHighlightEnable = highlightLineConfig.isHighlightLineEnabled;
+            this.highlightLine(this.decorationType, true)
+        })
+    }
+
+    public highlightLine(decorationType: any, updateAllVisibleEditors=false) {
+        const activeEditor = window.activeTextEditor;
+        const lastActivePosition = this.lastActivePosition;
+
+		try {
+			if (updateAllVisibleEditors) {
+				window.visibleTextEditors.forEach((editor) => {
+					const currentPosition = editor.selection.active;
+                    const newDecoration = { range: new Range(currentPosition, currentPosition) };
+                    editor.setDecorations(decorationType, [newDecoration]);
+                });
+            } else {
+				window.visibleTextEditors.forEach((editor) => {
+					if(editor !== window.activeTextEditor || activeEditor === undefined || lastActivePosition === undefined) return;
+
+                    const currentPosition = editor.selection.active
+                    const editorHasChangedLines = lastActivePosition.line !== currentPosition.line
+                    const isNewEditor = activeEditor.document.lineCount === 1 && lastActivePosition.line === 0 && lastActivePosition.character == 0;
+                    const newDecoration = { range: new Range(currentPosition, currentPosition) }
+
+                    if(editorHasChangedLines || isNewEditor){
+                        editor.setDecorations(decorationType, [newDecoration])
+                    }
+                });
+            }
+        }
+        catch (error){
+            console.error("Error from ' updateDecorations' -->", error)
+        } finally {
+			if (activeEditor === undefined) return;
+
+            this.lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+        }
+    }
+}
+
+export default HighlightLine
